7910caa4ca880086b2f5d4fc73c860bd3297aeca
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-24 08:35:04 -0400

    Revise catActions

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,13 +1,30 @@
+import fetch from 'isomorphic-fetch';
+
 export function fetchCats() {
-  return (dispatch) ={
-    dispatch({type: 'FETCH_CATS' })
-    return fetch('http://localhost:4000/db').then(response => {
-      return response.json()
-      }).then(responseJSON => {
-        return responseJSON.images
-      })
+
+  return function(dispatch){
+    dispatch({type: 'LOADING_CATS'})
+    return fetch('http://localhost:4000/db')
+      .then(res => {
+        return res.json()
+      }).then(responseJson => {
+        dispatch({type: 'FETCH_CATS', payload: responseJson.images})
+    })
+    // return cats;
   }
 }
+
+// Mine:
+// export function fetchCats() {
+//   return (dispatch) =>{
+//     dispatch({type: 'FETCH_CATS' })
+//     return fetch('http://localhost:4000/db').then(response => {
+//       return response.json()
+//       }).then(responseJSON => {
+//         return responseJSON.images
+//       })
+//   }
+// }
 
 // export function fetchCats() {
 //   return (dispatch) => {
