b428344a00625ea06b84e99b38de9362068615e9
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-23 20:08:57 -0400

    Add basic language to actions/catActions

---
+++ src/actions/catActions.js
@@ -0,0 +1,19 @@
+export function fetchCats() {
+  return (dispatch) ={
+    dispatch({type: 'FETCH_CATS' })
+    return fetch('http://localhost:4000/db').then(response => {
+      return response.json()
+      }).then(responseJSON => {
+        return responseJSON.images
+      })
+  }
+}
+
+// export function fetchCats() {
+//   return (dispatch) => {
+//     dispatch({ type: 'START_ADDING_CATS_REQUEST' });
+//     return fetch('http://www.catapi.com')
+//       .then(response => response.json())
+//       .then(cats => dispatch({ type: 'ADD_CATS', cats }));
+//   };
+// }
