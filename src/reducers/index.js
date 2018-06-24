7d6074ea9ff07c7701de4f6e049704ab6cbeb713
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-24 08:39:20 -0400

    Revise index.js with rootreducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,18 @@
+import { combineReducers } from 'redux';
+import catsReducer from './cats_reducer';
+
+
+const rootReducer =  combineReducers({
+  cats: catsReducer
+});
+
+
+export default rootReducer;
+
+
 // Blank at start of lab.  I add:
 
-import { combineReducers } from 'redux'
-import catsReducer from './cats_reducer'
-
-export default combineReducers({catsReducer})
+// import { combineReducers } from 'redux'
+// import catsReducer from './cats_reducer'
+//
+// export default combineReducers({catsReducer})
